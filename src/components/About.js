d1ac404ed994ca86daa10401ff91f0c601387bff
pbelokon <pavelbelokon786@gmail.com>
2025-10-06 23:45:55 -0400

    fix for upscaped symbols

--- src/components/About.js
+++ src/components/About.js
@@ -7,26 +7,27 @@ const asap = Asap({
 const About = () => {
   return (
     <section className="about-section">
-      <h1 className={asap.className}>Hi there! I'm Pavel.</h1>
+      <h1 className={asap.className}>Hi there! I&apos;m Pavel</h1>
       <div className="about-text">
         <p>
           I got interested in programming in 2017, when I created an
-          <span> evasion game with pygame </span>for my computer-science class,
-          given it's boundless creative possibilities I loved in since then.
+          <span> evasion game with pygame </span>
+          for my computer science class. Given its boundless creative
+          possibilities, I&apos;ve loved it since then.
         </p>
         <br />
         <p>
-          I've graduate from Seneca College in 2024 from Computer Science
-          degree, I've also interned at Toronto District School Board(TDSB){" "}
-          <br />
+          I&apos;ve graduated from Seneca College in 2024 with a Computer
+          Science degree. I&apos;ve also interned at Toronto District School
+          Board (TDSB) <br />
           as a <span>Web developer</span>.
         </p>
         <br />
         <p>
-          These days, I focus on facilitating my knowledge and skills in
-          <span> creative Computer Programming</span>, and I'm also working on a
-          <span> Game</span> for a <span> Game Studio </span> I started with my
-          friend.
+          These days, I focus on developing my knowledge and skills in
+          <span> creative Computer Programming</span>, and I&apos;m also working
+          on a<span> game</span> for a <span>game studio</span> I started with
+          my friend.
         </p>
       </div>
     </section>
